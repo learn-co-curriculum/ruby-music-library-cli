64a7fc1257b16648791ff4058b32851c75e70e42
Gabe Jackson <gj@mail.co.de>
2017-06-02 15:48:55 -0400

    update more test + heading descriptions

--- spec/004_songs_and_artists_spec.rb
+++ spec/004_songs_and_artists_spec.rb
@@ -6,19 +6,23 @@ describe "Associations — Song and Artist:" do
     @artist = Artist.new("Neutral Milk Hotel")
   end
 
-  context "Artists" do
-    it "are initialized with a 'songs' property set to an empty array (artist has many songs)" do
-      expect(@artist.instance_variable_defined?(:@songs)).to be(true)
-      expect(@artist.instance_variable_get(:@songs)).to eq([])
+  context "Artist" do
+    describe "#initialize" do
+      it "Artists are initialized with a 'songs' property set to an empty array (artist has many songs)" do
+        expect(@artist.instance_variable_defined?(:@songs)).to be(true)
+        expect(@artist.instance_variable_get(:@songs)).to eq([])
+      end
     end
 
-    it "can push a song into their 'songs' collection (artist has many songs)" do
-      @artist.songs << @song
-      expect(@artist.songs).to include(@song)
+    describe "#songs" do
+      it "pushes a song into the artist's 'songs' collection (artist has many songs)" do
+        @artist.songs << @song
+        expect(@artist.songs).to include(@song)
+      end
     end
   end
 
-  context "Songs" do
+  context "Song" do
     describe "#artist" do
       it "returns the artist of the song (song belongs to artist)" do
         @song.instance_variable_set(:@artist, @artist)
@@ -32,7 +36,7 @@ describe "Associations — Song and Artist:" do
         expect(@song.artist).to eq(@artist)
       end
 
-      it "adds the song to the artist's 'songs' collection (artist has many songs)" do
+      it "adds the song to the artist's 'songs' collection" do
         @song.artist = @artist
         expect(@artist.songs).to include(@song)
       end
@@ -41,7 +45,7 @@ describe "Associations — Song and Artist:" do
 
   context "adding a song to an artist" do
     describe "#add_song" do
-      it "adds the song to the artist's song collection" do
+      it "adds the song to the artist's song collection (artist has many songs)" do
         @artist.add_song(@song)
         expect(@artist.songs).to include(@song)
       end
