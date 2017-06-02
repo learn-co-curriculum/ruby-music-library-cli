f6a6d846623a134fb7f33c49599773e2da7c705a
Gabe Jackson <gj@mail.co.de>
2017-06-02 17:13:00 -0400

    use before(:each) + @instance vars to cut down on repetition

--- spec/005_songs_and_genres_spec.rb
+++ spec/005_songs_and_genres_spec.rb
@@ -1,63 +1,55 @@
 require "spec_helper"
 
 describe "Associations — Song and Genre:" do
+  before(:each) do
+    @song = Song.new("In the Aeroplane Over the Sea")
+    @genre = Genre.new("indie rock")
+  end
+
   context "Genre" do
     describe "#initialize" do
       it "creates a 'songs' property set to an empty array (genre has many songs)" do
-        genre = Genre.new("indie rock")
-
-        expect(genre.songs).to eq([])
+        expect(@genre.songs).to eq([])
       end
     end
 
     it "can push a song into the genre's song collection" do
-      genre = Genre.new("indie rock")
-      song = Song.new("In the Aeroplane Over the Sea")
-
-      genre.songs << song
-      expect(genre.songs).to include(song)
+      @genre.songs << @song
+      expect(@genre.songs).to include(@song)
     end
   end
 
   context "Songs belong to a genre" do
     describe "#genre=" do
       it "accepts an genre for the song" do
-        song = Song.new("In the Aeroplane Over the Sea")
-        genre = Genre.new("indie rock")
-        song.genre = genre
+        @song.genre = @genre
 
-        expect(song.genre).to eq(genre)
+        expect(@song.genre).to eq(@genre)
       end
 
       it "adds the song to the genre's songs" do
-        song = Song.new("In the Aeroplane Over the Sea")
-        genre = Genre.new("indie rock")
-        song.genre = genre
+        @song.genre = @genre
 
-        expect(genre.songs).to include(song)
+        expect(@genre.songs).to include(@song)
       end
 
       it "does not add the song to the genre's songs if it already exists" do
-        song = Song.new("In the Aeroplane Over the Sea")
-        genre = Genre.new("indie rock")
+        @song.genre = @genre
+        @song.genre = @genre
 
-        song.genre = genre
-        song.genre = genre
-
-        expect(genre.songs).to include(song)
-        expect(genre.songs.size).to eq(1)
+        expect(@genre.songs).to include(@song)
+        expect(@genre.songs.size).to eq(1)
       end
     end
   end
 
   context "initializing a song with a genre" do
     it "songs accept an optional argument for the genre" do
-      genre = Genre.new("indie rock")
       artist = Artist.new("Neutral Milk Hotel")
-      song = Song.new("In the Aeroplane Over the Sea", artist, genre)
+      song = Song.new("In the Aeroplane Over the Sea", artist, @genre)
 
-      expect(genre.songs).to include(song)
-      expect(song.genre).to eq(genre)
+      expect(@genre.songs).to include(song)
+      expect(song.genre).to eq(@genre)
     end
   end
 end
