b3ed0a97248da27950646b63f1cbd6fb365688cc
Gabe Jackson <gj@mail.co.de>
2017-06-02 17:51:14 -0400

    make clear which artist we're talking about

--- spec/004_songs_and_artists_spec.rb
+++ spec/004_songs_and_artists_spec.rb
@@ -50,7 +50,7 @@ describe "Associations — Song and Artist:" do
 
   context "Artist" do
     describe "#add_song" do
-      it "assigns the artist to the song's 'artist' property (song belongs to artist)" do
+      it "assigns the current artist to the song's 'artist' property (song belongs to artist)" do
         @artist.add_song(@song)
         expect(@song.artist).to be(@artist)
       end
